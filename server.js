aa8a2875b0cd549f64f21983df650a3acde83210
Tapan <tapanpatel09@gmail.com>
2021-03-25 09:57:17 -0400

    Initial commit

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema');
 const app = express();
+const cors = require('cors');
+//Allow cross-origin
+
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:true
 }));
+app.use(express.static('public'));
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','index.html'));
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
